3f9abe29ca2b00559af57c68726f08a50b303fd1
Csaba Sipocz <glinto@gmail.com>
2021-03-19 14:58:13 +0100

    Initial commit

---
+++ crc-test.py
@@ -0,0 +1,30 @@
+def crc16_modbus(buf: bytearray) -> int:
+    # Until we implement the CRC16 algorithm properly, our code will break at the assertions 
+    return 0
+
+# Our test strings
+phrases = [
+    'Assorted Breath',
+    'Robust Arch',
+    'Prickly Scent',
+    'Bustling Lumber',
+    'Graceful Ticket'
+]
+
+# And their checksum, respecitvely
+checksums = [
+    17040,
+    10013,
+    55244,
+    5881,
+    10582
+]
+
+# Check if our CRC algorithm 
+for i in range(len(phrases)): 
+    print(phrases[i], end='... ') 
+    checksum = crc16_modbus(bytearray(phrases[i], 'utf-8'))
+    assert checksum == checksums[i]
+    print('ok!')
+
+
